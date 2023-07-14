357ef9a20556280857942d2b93618669ee658b1e
Parsa Kzr <54034132+parsakzr@users.noreply.github.com>
2023-07-14 22:06:33 +0300

    init nanograd package

---
+++ nanograd/engine.py
@@ -0,0 +1,101 @@
+import math
+import numpy as np
+
+class Value:
+
+    def __init__(self, data, _children=(), _op='', label=''):
+        self.data = data
+        self.grad = 0.0
+        self._backward = lambda: None
+        self._prev = set(_children)
+        self._op = _op
+        self.label = label
+
+    
+    def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out =  Value(self.data + other.data, (self, other), '+')
+        def _backward():
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
+        out._backward = _backward
+        return out
+    
+    
+    def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data * other.data, (self, other), '*')
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+        out._backward = _backward
+        return out
+    
+    
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)), "only supporting int/float powers for now"
+        out = Value(self.data**other, (self,), f'**{other}')
+        def _backward():
+            self.grad += (other*self.data**(other-1)) * out.grad
+        out._backward = _backward
+        return out
+
+    
+    
+    
+    def exp(self):
+        out = Value(math.exp(self.data), (self,), 'exp')
+
+        def _backward():
+            self.grad += out.data * out.grad
+        out._backward = _backward
+        return out
+    
+    def tanh(self):
+        n = self.data
+        t = (math.exp(2*n)-1) / (math.exp(2*n)+1)
+        out = Value(t, (self,), 'tanh')
+        def _backward():
+            self.grad += (1 - t**2) * out.grad
+        out._backward = _backward
+        return out
+    
+    def backward(self): # refer to ### Automatic backpropagation cells
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+
+        self.grad = 1.0
+        for v in reversed(topo):
+            v._backward()
+
+
+    def __radd__(self, other): # other + self
+        return self + other
+    
+    def __rmul__(self, other): # other * self
+        return self * other
+    
+    def __neg__(self):
+        return self * -1
+    
+    def __sub__(self, other):
+        return self + (-other)
+    
+    def __rsub__(self, other):
+        return other + (-self)
+    
+    def __truediv__(self, other):
+        return self * other**-1
+    
+    def __rtruediv__(self, other):
+        return other * self**-1
+    
+    def __repr__(self) -> str:
+        return f"Value({self.data})"
